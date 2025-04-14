112f7291a85484497e1854eb07b47bda5ddf6918
John Ndungu <ndungulink@gmail.com>
2025-04-14 15:29:17 +0300

    Linking the communication between the frontend and the backend

--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -37,18 +37,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'corsheaders', #Added for linking up with frontend
+    'api',  #added for the app
+    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS[
+    'http://localhost:5173' # the added url
+]
+
 
 ROOT_URLCONF = 'crud.urls'
 
