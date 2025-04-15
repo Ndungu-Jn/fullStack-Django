4997339355ed2dd18eedfdfdb7b7b550deb154f8
John Ndungu <ndungulink@gmail.com>
2025-04-15 09:51:00 +0300

    Basic template format. Starter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,21 @@ import Home from "./components/home";
 import Create from "./components/Create";
 import Edit from "./components/Edit";
 import Delete from "./components/Delete";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="" element={<Home />}></Route>
-        <Route path="/create" element={<Create />}></Route>
-        <Route path="/edit/:id" element={<Edit />}></Route>
-        <Route path="/delete/:id" element={<Delete />}></Route>
-      </Routes>
+      <Navbar
+        content={
+          <Routes>
+            <Route path="" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+            <Route path="/edit/:id" element={<Edit />}></Route>
+            <Route path="/delete/:id" element={<Delete />}></Route>
+          </Routes>
+        }
+      />
     </>
   );
 }
