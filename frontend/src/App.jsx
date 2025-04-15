aad0112545013b5c2d8b5c9ecfe95993d74f6380
John Ndungu <ndungulink@gmail.com>
2025-04-15 08:42:07 +0300

    Applied Routing in the app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
-
+import { Routes, Route } from "react-router";
 import "./App.css";
+import Home from "./components/home";
+import Create from "./components/Create";
+import Edit from "./components/Edit";
+import Delete from "./components/Delete";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<Home />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
+        <Route path="/delete/:id" element={<Delete />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
