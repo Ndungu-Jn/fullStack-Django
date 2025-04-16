32f7acaf5751d00d3052748dad66f99bc7cb679a
John Ndungu <ndungulink@gmail.com>
2025-04-16 10:05:21 +0300

    Created API using django rest framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions  # the use of viewsets
+from .serializers import * #get all serializers
+from .models import *   #get all models
+from rest_framework.response import Response
 # Create your views here.
+
+class CountryViewset(viewsets.ViewSet):
+    permissions_classes = [permissions.AllowAny] # giving permission to whos gonna access the site
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+    def list(self, request):
+        queryset = Country.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+    
+class LeagueViewset(viewsets.ViewSet):
+    permissions_classes = [permissions.AllowAny] # giving permission to whos gonna access the site
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+
+    def list(self, request):
+        queryset = League.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+    
+class CharacteristicViewset(viewsets.ViewSet):
+    permissions_classes = [permissions.AllowAny] # giving permission to whos gonna access the site
+    queryset = Characteristic.objects.all()
+    serializer_class = CharacteristicSerializer
+
+    def list(self, request):
+        queryset = Characteristic.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
